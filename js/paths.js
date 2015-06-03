06e381ff69968085672cefaf2181ab1f9d10a142
Kalimaha <guido.barbaglia@gmail.com>
2015-06-03 08:42:31 +0200

    Create a 'By Country' module for the Browse section. closes #27

---
+++ js/paths.js
@@ -0,0 +1,17 @@
+define(function() {
+
+    var config = {
+        paths: {
+            FAOSTAT_UI_BROWSE_BY_COUNTRY: 'faostat-ui-browse-by-country',
+            faostat_ui_browse_by_country: '../'
+        },
+        shim: {
+            bootstrap: {
+                deps: ['jquery']
+            }
+        }
+    };
+
+    return config;
+
+});
---
+++ nls/translate.js
@@ -0,0 +1,7 @@
+define({
+    root : {
+        error: 'FAOSTAT Browse By Country Error'
+    },
+    fr : true,
+    es : true
+});
